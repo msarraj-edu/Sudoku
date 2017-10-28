ec6f40eb38fe715a884a35d96a27490badbfeeb5
labuser <labuser@labusers-air.kaust.edu.sa>
2017-10-28 21:19:31 +0300

    Print sudoku grid

--- Sudoku/Sudoku/GameViewController.swift
+++ Sudoku/Sudoku/GameViewController.swift
@@ -14,7 +14,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let brain = SudokuBrain()
+        print(brain.getGrid())
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
